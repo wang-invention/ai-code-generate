8ef36dfabf6284b921841a8e3e640628e31aec1b
wanginvention <18279325282@163.com>
2026-01-01 15:31:23 +0800

    first commit

---
+++ src/main/java/com/wang/wangaicodemother/WangAiCodeMotherApplication.java
@@ -0,0 +1,13 @@
+package com.wang.wangaicodemother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WangAiCodeMotherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WangAiCodeMotherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wang/wangaicodemother/WangAiCodeMotherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wang.wangaicodemother;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WangAiCodeMotherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
